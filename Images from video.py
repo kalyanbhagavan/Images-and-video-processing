c4619e4c3bb822d2e1c17cb996008e0ab0a93f11
Kalyan Bhagavan <kalyanbhagavan@github.com>
2019-05-29 23:00:48 +0200

    Images from Video

---
+++ Images from video.py
@@ -0,0 +1,13 @@
+import cv2
+print(cv2.__version__)
+videocap = cv2.VideoCapture('12.mp4')
+success, image = videocap.read()
+count = 0
+success = True
+while success:
+    if count >= 50:
+        ind = count-50
+        cv2.imwrite("frame%d.png" % ind, image)     # save frame as PNG file
+    success, image = videocap.read()
+    print('Read a new frame: ', success)
+    count += 1
